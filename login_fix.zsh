62b006c3a5aea178b7956fd30d7ba080089e57d9
Ali Medina <amedina@student.1337.ma>
2026-06-11 17:15:42 +0100

    aa

--- login_fix.zsh
+++ login_fix.zsh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 source $SCRIPT_DIR/wizard.zsh
 
@@ -9,14 +9,22 @@ reload() {
     source ~/.bashrc
 }
 
-if ! [ -f ~/.Catalina_Scripts.sh] &> /dev/null; then
-    touch .Catalina_Scripts.sh
+Interactive_wizard(){
+# ##Install Dependencies
+    brew_wizard
+    openssl_wizard
+    python_wizard
+    app_installer_wizard
+}
+
+if ! [ -f ~/.Catalina_Scripts.sh ]; then
+    touch /Users/$USER/.Catalina_Scripts.sh
     echo "source /Users/$USER/.Catalina_Scripts.sh" >> /Users/$USER/.zshrc
     echo "source /Users/$USER/.Catalina_Scripts.sh" >> /Users/$USER/.bashrc
     reload
 fi
 
-#Prerequisites
+# Prerequisites
 printf "You can run the script in 2 ways.\n"
 printf "0: [Setup a 'config.env' with all the parameters setup]\n"
 printf "1: [Setup the Script now in an Interactive wizard]\n"
@@ -44,13 +52,6 @@ case $setup_mode in
     ;;
 esac
 
-Interactive_wizard(){
-# ##Install Dependencies
-    brew_wizard
-    openssl_wizard
-    python_wizard
-    app_installer_wizard
-}
 #Startup agent Setup
 
 #Storage Cleaner
